87a6ba1d635c06e17c84f53d20c9764708b03e7d
Sheila R Moore <sherexmykes@github.com>
2019-06-08 14:24:35 -0400

    added more listItems

--- starwars/src/components/CharCards.js
+++ starwars/src/components/CharCards.js
@@ -9,7 +9,13 @@ const CharCards = props => {
         <p className="listItem">Height:{props.data.height}</p>
         <p className="listItem">Skin Color:{props.data.skin_color}</p>
         <p className="listItem">Date of Birth:{props.data.birth_year}</p>
-
+        <p className="listItem">Created:{props.data.created}</p>
+        <p className="listItem">Edited:{props.data.edited}</p>
+        <p className="listItem">Homeworld:{props.data.homeworld}</p>
+        <p className="listItem">Species:{props.data.species}</p>
+        <p className="listItem">Starships:{props.data.starships}</p>
+        <p className="listItem">URL:{props.data.url}</p>
+        <p className="listItem">Vehicles:{props.data.vehicles}</p>
    </div>
     )       
 }
